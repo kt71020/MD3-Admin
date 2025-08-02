aafb812354fe7393bb4fb8fc7199f386ae4801b1
KT Lin <kt71020@gmail.com>
2025-08-02 23:18:44 +0800

    新增 FilePickerPlugin 至 GeneratedPluginRegistrant.swift，以支援檔案選擇功能，擴展應用程式的功能性。

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
 import firebase_auth
 import firebase_core
 import firebase_messaging
@@ -15,6 +16,7 @@ import sign_in_with_apple
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
